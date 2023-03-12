761e60cbeefa0a362d7adb1401fcf523bf6b8c60
Temple <clemtmpl@gmail.com>
2023-03-12 16:44:55 +0100

    First pull with loaderPage

---
+++ app/src/main/java/com/example/mobileproject/GroupInfosActivty.kt
@@ -0,0 +1,11 @@
+package com.example.mobileproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class GroupInfosActivty : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_group_infos_activty)
+    }
+}
